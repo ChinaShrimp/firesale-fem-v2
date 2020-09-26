454e7a798d5dd84a40d61aecbf4be67e81a52bd8
梁亮 <liang.liang@cctrend.com.cn>
2020-09-26 16:10:26 +0800

    open file

--- app/main.js
+++ app/main.js
@@ -1,1 +1,77 @@
-console.log('This is an Electron application!');
+const fs = require('fs')
+const {
+    app,
+    BrowserWindow,
+    dialog
+} = require('electron')
+
+let mainWindow = null
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        show: false
+    })
+
+    mainWindow.loadFile(`${__dirname}/index.html`)
+
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show()
+    })
+})
+
+console.log('Starting up...')
+
+exports.dispatch = (action) => {
+    const {
+        type,
+        payload
+    } = action
+    const {
+        replyTopic
+    } = payload;
+    const result = null;
+
+    const cb = (...args) => {
+        if (replyTopic) {
+            mainWindow.webContents.send(replyTopic, ...args)
+        }
+    }
+
+    switch (type) {
+        case 'openFile':
+            result = getFileFromUser(payload, cb)
+            break
+
+        default:
+            console.log("Unsupported Action Type")
+    }
+}
+
+const getFileFromUser = ({
+    properties,
+    filters
+}, cb) => {
+    const files = dialog.showOpenDialog({
+        properties: properties || ['openFile'],
+        filters: filters || [{
+                name: "Markdown Files",
+                extensions: ['md', 'markdown']
+            },
+            {
+                name: 'Text Files',
+                extensions: ['text', 'txt']
+            },
+            {
+                name: 'JSON Files',
+                extensions: ['json']
+            }
+        ]
+    })
+
+    if (!files) cb()
+
+    let file = files[0]
+    const content = fs.readFileSync(file).toString()
+
+    cb(file, content)
+}
--- app/renderer.js
+++ app/renderer.js
@@ -1,3 +1,9 @@
+const {
+  remote,
+  ipcRenderer
+} = require('electron')
+
+const mainProcess = remote.require('./main.js')
 const marked = require('marked');
 
 const markdownView = document.querySelector('#markdown');
@@ -11,10 +17,39 @@ const showFileButton = document.querySelector('#show-file');
 const openInDefaultButton = document.querySelector('#open-in-default');
 
 const renderMarkdownToHtml = markdown => {
-  htmlView.innerHTML = marked(markdown, { sanitize: true });
+  htmlView.innerHTML = marked(markdown, {
+    sanitize: true
+  });
 };
 
 markdownView.addEventListener('keyup', event => {
   const currentContent = event.target.value;
   renderMarkdownToHtml(currentContent);
-});
+})
+
+openFileButton.addEventListener('click', () => {
+  dispatchToMainProcess('openFile', {}, (file, content) => {
+    if (file && content) {
+      markdownView.value = content
+      renderMarkdownToHtml(content)
+    }
+  })
+})
+
+const dispatchToMainProcess = (type, payload, cb) => {
+  let replyTopic = type + "-reply"
+
+  let target = ipcRenderer.on(replyTopic, (ev, ...args) => {
+    cb(...args)
+
+    target.removeListener(replyTopic)
+  })
+
+  mainProcess.dispatch({
+    type,
+    payload: {
+      replyTopic,
+      ...payload
+    }
+  })
+}
